0d8550969af435f1f41bf2b298886c5d7075fc32
Eliana Pacheco <elitapachecor@gmail.com>
2019-03-30 16:34:52 -0500

    Mi aplicación Django Girls, primer commit

---
+++ probando python.py
@@ -0,0 +1,21 @@
+#Aquí voy a crear una variable con mi nombre
+Nombre = 'Eliana'
+Telefono = 981699253
+Ubicacion = 'Cuenca'
+Actividad = 'Programando'
+Edad = 20
+Recomendacion = 'Poner buena musica'
+
+#Ahora voy a utilizar las condiciones
+if(Edad>18):
+	#Imprimir resultado final
+	print('Mi nombre es ' +  Nombre  + ' Mi numero de telefono es ' + str( Telefono )  + ' Estoy en ' 
+	+ Ubicacion + ' y estoy ' +  Actividad  + ' Y mi recomendacion para el proximo Django girls es '  +  Recomendacion)
+else:
+	print('No fui aceptada en el Django girls')
+
+
+
+
+
+
